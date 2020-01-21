233662e33625d06c28dfb6a8f97aeb97a7d08a98
Dominik Soczewka <dominiksoczewka@Dominiks-MacBook-Pro.local>
2020-01-21 22:27:07 +0100

    styling wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'transaction.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,30 +9,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Expanses app'),
-        ),
-        body: Column( // takes the width of the child
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          // because the other Card takes all the space available, Column also expands and therefore second card can take that space as well
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            // container so that width control would be possible
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text('CHART!'),
-                color: Colors.blue,
-                elevation: 5,
-              ),
+      title: 'Expenses App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Wekly groceries', amount: 13.15, date: DateTime.now())
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expenses app'),
+      ),
+      body: Column(
+        // takes the width of the child
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // because the other Card takes all the space available, Column also expands and therefore second card can take that space as well
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // container so that width control would be possible
+          Container(
+            width: double.infinity,
+            child: Card(
+              child: Text('CHART!'),
+              color: Colors.blue,
+              elevation: 5,
             ),
-            Card(
-              child: Text('LIST OF TX'),
-              color: Colors.red,
-            ),
-          ],
-        ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2)),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tx.amount.toString(),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(tx.title),
+                        Text(tx.date.toString()),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
